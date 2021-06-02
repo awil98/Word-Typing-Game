12b542f4276a2ab66330900e3e02ae1bbdb79ab8
Aaron Williams <34403759+awil98@users.noreply.github.com>
2021-06-02 11:56:10 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,56 +1,16 @@
 import React, {useState, useEffect, useRef} from "react"
-
-/**
- * Challenge:
- * 
- * Move the "business logic" into a custom hook, which will provide
- * any parts of state and any functions to this component to use.
- * 
- * You can easily tell which parts the component needs by looking at 
- * the variables being used inside the `return`ed markup below.
- */
+import useWordGame from "./hooks/useWordGame"
 
 function App() {
-    const STARTING_TIME = 5
-    
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textBoxRef = useRef(null)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-    
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-        textBoxRef.current.disabled = false
-        textBoxRef.current.focus()
-    }
-    
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning])
+    const {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+    } = useWordGame(5)
     
     return (
         <div>
